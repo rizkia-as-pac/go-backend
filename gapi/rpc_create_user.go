42646838f8cf5ba29bb5dbecfe67ecc0fd6f439e
rizkia-as-pac <rizkia.as.pac@gmail.com>
2023-09-02 16:34:01 +0900

    Validate gRPC parameters and send human/machine friendly response

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -6,14 +6,20 @@ import (
 	db "github.com/tech_school/simple_bank/db/sqlc"
 	"github.com/tech_school/simple_bank/pb"
 	"github.com/tech_school/simple_bank/utils/pass"
+	"github.com/tech_school/simple_bank/validator"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
 	// jika pada http server kita perlu melakukan bind pada request yang dikirimkan client. pada grpc framework itu semua sudah otomatis
-
 	// grpc has already bound all the input data into req object for us
+
+	// if violations not nil then it means that there's atleast one invalid parameter
+	if violations := validateCreateUserRequest(req); violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 
 	// hashedPassword, err := pass.HashedPassword(req.Password)
 	hashedPassword, err := pass.HashedPassword(req.GetPassword()) // // bisa seperti atas, tapi menggunakan req.GetPassword() lebih baik karna sudah ada pengecekan terlebih dahulu passwordnya, just in case the request field is nil
@@ -44,4 +50,24 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 	}
 
 	return res, nil
+}
+
+func validateCreateUserRequest(req *pb.CreateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := validator.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, filedViolation("username", err))
+	}
+
+	if err := validator.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, filedViolation("password", err))
+	}
+
+	if err := validator.ValidateFullName(req.GetFullName()); err != nil {
+		violations = append(violations, filedViolation("full_name", err))
+	}
+
+	if err := validator.ValidateEmail(req.GetEmail()); err != nil {
+		violations = append(violations, filedViolation("email", err))
+	}
+
+	return violations
 }
---
+++ validator/validator.go
@@ -0,0 +1,63 @@
+package validator
+
+import (
+	"fmt"
+	"net/mail"
+	"regexp"
+)
+
+var (
+	isValidUsername = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString // matchstring tanpa () berarti sekarang variable isvalidusername menjadi function
+	isValidFullname = regexp.MustCompile(`^[a-zA-Z\s]+$`).MatchString
+)
+
+// general function to check if a string has an appropriate length or not
+func ValidateString(value string, minLength int, maxLength int) error {
+	n := len(value)
+	if n < minLength || n > maxLength {
+		return fmt.Errorf("must contain from %d-%d characters", minLength, maxLength)
+	}
+
+	return nil
+}
+
+func ValidateUsername(name string) error {
+	if err := ValidateString(name, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidUsername(name) {
+		return fmt.Errorf("must contain only lowercase letters, digits or underscore")
+	}
+
+	return nil
+}
+
+func ValidateFullName(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidFullname(value) {
+		return fmt.Errorf("must contain only letters or spaces")
+	}
+
+	return nil
+}
+
+func ValidatePassword(value string) error {
+	return ValidateString(value, 4, 100)
+}
+
+func ValidateEmail(value string) error {
+	if err := ValidateString(value, 3, 200); err != nil {
+		return err
+	}
+
+	// function ini mengembalikan parse email address dan error, kita akan memanfaatkan return errornya untuk check validitas email
+	if _, err := mail.ParseAddress(value); err != nil {
+		return fmt.Errorf("is not valid email address")
+	}
+
+	return nil
+}
